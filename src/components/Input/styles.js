b7b06e3b653062cd0057c321daa96184857909da
luisevilela <luisevilela@gmail.com>
2022-12-14 16:31:16 -0300

    Criação da Calculadora da DIO Curso Orange

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div `
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+    
+    display: flex;
+    align-itens: center;
+    justify-contents: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+      width: 100%
+      height: 75px;
+      background-color: #ef4226;
+      border: 0;
+      display: flex;
+      flex-direction: column;
+      align-itens: flex-end;
+      padding: 0 10px;
+      font-size: 28px;
+      font-weight: 500;
+      font-family: 'Roboto';
+      color: #F2F2F2F2;
+    }
+`
